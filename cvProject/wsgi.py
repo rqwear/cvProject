f049e4d198b247cb41fcfdd221101d9fbac85ee1
rqwear <avatrx@gmail.com>
2021-09-23 17:04:34 +0300

    secret key

--- cvProject/wsgi.py
+++ cvProject/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+
+project_folder = os.path.expanduser('~/cvProject')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cvProject.settings')
 
