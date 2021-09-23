639e58742dc5f3240ec89fb8f431d376c03402cf
rqwear <avatrx@gmail.com>
2021-09-23 18:28:53 +0300

    sqlite3 changes

--- cvProject/settings.py
+++ cvProject/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'cvProject.wsgi.application'
 
 # Database chose sqlite3 or postgresql
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
+#DATABASES = {
+ #   'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+ #       'NAME': os.getenv('DB_NAME'),
+ #       'USER': os.getenv('DB_USER'),
+ #       'PASSWORD': os.getenv('DB_PASS'),
+ #       'HOST': os.getenv('DB_HOST'),
+ #       'PORT': os.getenv('DB_PORT'),
+ #   }
+#}
 
 
 # Password validation
